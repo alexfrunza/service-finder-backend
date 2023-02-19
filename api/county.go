50928d052190d91077263094f6e76383ca254d42
Alexandru Frunza <frunza.alexandru.2004@gmail.com>
2023-02-19 12:44:32 +0200

    Add filter to counties by country id

--- api/county.go
+++ api/county.go
@@ -1,14 +1,17 @@
 package api
 
 import (
+	"fmt"
 	"github.com/alexfrunza/servicefinder/db"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"strconv"
 )
 
 type county struct {
-	Name string `json:"name"`
-	Id   int    `json:"id"`
+	Name      string `json:"name"`
+	Id        int    `json:"id"`
+	CountryId int    `json:"countryId"`
 }
 
 type resCounties struct {
@@ -17,13 +20,26 @@ type resCounties struct {
 }
 
 func getCounties(c echo.Context) error {
+	countQuery := `SELECT COUNT(*) FROM counties `
+	rowsQuery := `SELECT id, name, country_id  FROM counties `
+
+	if c.QueryParam("countryId") != "" {
+		countryId, err := strconv.Atoi(c.QueryParam("countryId"))
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, &resBodyError{Errors: []apiError{{Message: "This field must be an integer", Param: "countryId"}}})
+		}
+
+		countQuery = countQuery + fmt.Sprintf(`WHERE country_id=%d `, countryId)
+		rowsQuery = rowsQuery + fmt.Sprintf(`WHERE country_id=%d `, countryId)
+	}
+
 	var counter int
-	err := db.DB.QueryRow(`SELECT COUNT(*) FROM counties;`).Scan(&counter)
+	err := db.DB.QueryRow(countQuery).Scan(&counter)
 	if err != nil {
 		return err
 	}
 
-	rows, err := db.DB.Query(`SELECT id, name FROM counties;`)
+	rows, err := db.DB.Query(rowsQuery)
 	if err != nil {
 		return err
 	}
@@ -31,7 +47,7 @@ func getCounties(c echo.Context) error {
 	counties := make([]county, 0, counter)
 	for rows.Next() {
 		var c county
-		if err := rows.Scan(&c.Id, &c.Name); err != nil {
+		if err := rows.Scan(&c.Id, &c.Name, &c.CountryId); err != nil {
 			return err
 		}
 		counties = append(counties, c)
